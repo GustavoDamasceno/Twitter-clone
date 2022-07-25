a26a621952cae771e89dc7fd97df6802a334bb85
Damasceno <eng.damasceno@alu.ufc.br>
2022-07-25 15:48:55 -0300

    Add files via upload

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,12 @@
 import SlideMenu from "../../Components/slideMenu/"
 import SlideColumn from "../../Components/slideColumn/"
 import toptweet from "../../Images/toptweet.svg"
+import profileimg from "../../Images/Small.svg"
+import poll from "../../Images/poll.svg"
+import gif from "../../Images/gif.svg"
+import emoji from "../../Images/emoji.svg"
+import schedule from "../../Images/schedule.svg"
+import media from "../../Images/media.svg"
 import './styles.css'
 
 function Home() {
@@ -13,7 +19,21 @@ function Home() {
         <h2>Home</h2>
         <img src={toptweet}/>
       </div>
-      <div className="tweet"></div>
+
+      <div className="tweet">
+        <img className="blprofile" src={profileimg}/>
+        <textarea className="txttweet" placeholder="What’s happening?"/>
+        <div className="contbtns">
+          <button className='btns'><img src={media}/></button>
+          <button className='btns'><img src={gif}/></button>
+          <button className='btns'><img src={poll}/></button>
+          <button className='btns'><img src={emoji}/></button>
+          <button className='btns'><img src={schedule}/></button>
+        </div>
+        <div className="btntweet">Tweet</div>
+      </div>
+
+      <div className="separador"></div>
 
       <tr className="righttr"></tr>
       <SlideColumn/>
